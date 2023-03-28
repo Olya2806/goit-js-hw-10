f02243acb22adabaeab447b1802a987ab72e6914
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-03-28 15:55:55 +0300

    add css

--- src/index.js
+++ src/index.js
@@ -30,8 +30,9 @@ function onSearchCountryInput(e) {
                 wantedCounrtyInfoBox.innerHTML = '';
                 return
             }
-            if (data.length <= 10 || data.length >= 2) {
+            if (data.length <= 10 && data.length >= 2) {
                 coutryListEl.innerHTML = createCountryList(data);
+                wantedCounrtyInfoBox.innerHTML = '';
                 console.log(data);
                 return
             }
@@ -45,20 +46,22 @@ function onSearchCountryInput(e) {
         }).catch((error) => {
             console.log(error);
             Notiflix.Notify.failure('Oops, there is no country with that name');
+            coutryListEl.innerHTML = ""
+            wantedCounrtyInfoBox.innerHTML = '';
     })
 }
 
 
 function createCountryList(countries) {
     return countries.map((country) => `<li class="wanted-country_item">
-    <img src="${country.flags.svg}" alt="Flag" class="wanted-country_img" width="50"/>
+    <img src="${country.flags.svg}" alt="Flag" class="wanted-country_img" width="30" height="20" />
     <p class="wanted-country_name">${country.name.official}</p>
     </li>`).join("");
 }
 
 function createWantedCountryInfo(countries) {
     return countries.map((country) => `<ul class="one-country-info">
-    <li class="one-country-main-info"><img src="${country.flags.svg}" alt="Flag" class="one-country-img" width="50"/><p>${country.name.official}</p></li>
+    <li class="one-country-main-info"><img src="${country.flags.svg}" alt="Flag" class="one-country-img" width="50" height="30"/><p>${country.name.official}</p></li>
     <li class="one-country-item"><p><span class="one-country-text">Capital:</span> ${country.capital}</p></li>
     <li class="one-country-item"><p><span class="one-country-text">Population:</span> ${country.population}</p></li>
     <li class="one-country-item"><p><span class="one-country-text">Languages:</span> ${Object.values(country.languages)}</p></li>
